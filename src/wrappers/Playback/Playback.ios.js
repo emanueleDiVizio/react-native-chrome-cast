3fda50d24ad074cc0a96812b76321a93caad7d87
Nadia Dillon <dillon.nadia@gmail.com>
2017-11-07 14:09:32 +0000

    update: playback class

--- src/wrappers/Playback/Playback.ios.js
+++ src/wrappers/Playback/Playback.ios.js
@@ -5,11 +5,19 @@ const { GoogleCast } = NativeModules;
 const ChromecastEmitter = new NativeEventEmitter(GoogleCast);
 
 export default class ChromeCastPlayback {
-  onChromeCastMediaLoaded(cb) {
+  onChromeCastMediaLoaded = (cb) => {
     ChromecastEmitter.addListener(PlaybackStatus.MEDIA_LOADED, () =>
       cb({
         playing: true,
       }),
     );
+  }
+
+  startMediaLoadedLister(cb) {
+    this.mediaLoaded = this.onChromeCastMediaLoaded(cb);
+  }
+
+  stopMediaLoadedListener() {
+    this.mediaLoaded.remove();
   }
 }
