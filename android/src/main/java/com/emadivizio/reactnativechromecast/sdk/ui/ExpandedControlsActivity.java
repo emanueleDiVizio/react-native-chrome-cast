67a7d6852ce4cf2722fd504f504b2020ee69060c
Emanuele <Emanuele.d>
2017-11-07 15:33:08 +0000

    fixed crash

--- android/src/main/java/com/emadivizio/reactnativechromecast/sdk/ui/ExpandedControlsActivity.java
+++ android/src/main/java/com/emadivizio/reactnativechromecast/sdk/ui/ExpandedControlsActivity.java
@@ -1,11 +1,8 @@
 package com.emadivizio.reactnativechromecast.sdk.ui;
 
-import android.os.Bundle;
 import android.view.Menu;
 
 import com.emadivizio.reactnativechromecast.R;
-import com.emadivizio.reactnativechromecast.react.events.ReactCastScreenListener;
-import com.facebook.react.bridge.ReactContext;
 import com.google.android.gms.cast.framework.CastButtonFactory;
 import com.google.android.gms.cast.framework.media.widget.ExpandedControllerActivity;
 
@@ -15,28 +12,11 @@ import com.google.android.gms.cast.framework.media.widget.ExpandedControllerActi
 
 public class ExpandedControlsActivity extends ExpandedControllerActivity {
 
-  private ReactCastScreenListener reactCastScreenListener;
-
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
     super.onCreateOptionsMenu(menu);
     getMenuInflater().inflate(R.menu.cast_activity_menu, menu);
     CastButtonFactory.setUpMediaRouteButton(this, menu, R.id.media_route_menu_item);
     return true;
-  }
-
-
-  @Override
-  protected void onCreate(Bundle bundle) {
-    super.onCreate(bundle);
-    reactCastScreenListener = new ReactCastScreenListener((ReactContext) getApplicationContext());
-    reactCastScreenListener.onScreenOpen();
-  }
-
-
-  @Override
-  protected void onStop() {
-    super.onStop();
-    reactCastScreenListener.onScreenClose();
   }
 }
